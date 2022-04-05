d86f2d7ebad6621270059713d2563113dd56ad6d
aboodilatif <20840437+aboodilatif@users.noreply.github.com>
2022-04-05 03:26:50 +0300

    Create base html gauge
    - converted 748 FCU IAS selector display (html, js, css) to L1011
    - created display 3d model
    - created new panel.cfg & xml

---
+++ PackageSources/html_ui/Pages/VCockpit/Instruments/Airliners/L1011/AP/IAS/L1011_IAS.js
@@ -0,0 +1,5 @@
+class L1011_IAS extends Boeing_FCU.IAS {
+    get templateID() { return "L1011_IAS"; }
+}
+registerInstrument("l1011-ias-element", L1011_IAS);
+//# sourceMappingURL=B747_8_IAS.js.map
