37194ad69a7552ca13612e651e3dabfeb3bf9b1c
aboodilatif <20840437+aboodilatif@users.noreply.github.com>
2022-04-07 00:40:32 +0300

    Progress Update
    - continued cockpit modeling progress
    - AP panel displays test setup

---
+++ Packages/banditsim-aircraft-lockheed-l1011/html_ui/Pages/VCockpit/Instruments/Airliners/L1011/AP/ALT/L1011_ALT.js
@@ -0,0 +1,5 @@
+class L1011_ALT extends Boeing_FCU.ALT {
+    get templateID() { return "L1011_ALT"; }
+}
+registerInstrument("l1011-alt-element", L1011_ALT);
+//# sourceMappingURL=B747_8_ALT.js.map
---
+++ Packages/banditsim-aircraft-lockheed-l1011/html_ui/Pages/VCockpit/Instruments/Airliners/L1011/AP/HDG/L1011_HDG.js
@@ -0,0 +1,5 @@
+class L1011_HDG extends Boeing_FCU.HDG {
+    get templateID() { return "L1011_HDG"; }
+}
+registerInstrument("l1011-hdg-element", L1011_HDG);
+//# sourceMappingURL=B747_8_HDG.js.map
