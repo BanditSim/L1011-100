99948030739b263efcec34882ff615f3ff039ad1
aboodilatif <20840437+aboodilatif@users.noreply.github.com>
2023-11-26 16:51:27 +0300

    progress commit

--- Packages/banditsim-aircraft-lockheed-l1011/html_ui/Pages/VCockpit/Instruments/Airliners/L1011/FMC/B747_8_FMC_IdentPage.js
+++ Packages/banditsim-aircraft-lockheed-l1011/html_ui/Pages/VCockpit/Instruments/Airliners/L1011/FMC/B747_8_FMC_IdentPage.js
@@ -7,19 +7,19 @@ class FMCIdentPage {
         }
         let date = fmc.getNavDataDateRange();
         fmc.setTemplate([
-            ['RTE DATA     SV 7704'],
+            ['RTE DATA SV 7704'],
             ['PGM', '1612987-115'],
-            [''],
+            [''],/**/
             ['GMT', '', '23:48'],
+            [''],/**/
             [''],
+            [''],/**/
             [''],
+            [''],/**/
             [''],
+            [''],/**/
             [''],
-            [''],
-            [''],
-            [''],
-            [''],
-            ['GMT', '', '?'],
+            ['GMT', '', '?'],/**/
     ]);
         fmc.onLeftInput[5] = () => { B747_8_FMC_InitRefIndexPage.ShowPage1(fmc); };
         fmc.onRightInput[5] = () => { FMCPosInitPage.ShowPage1(fmc); };
